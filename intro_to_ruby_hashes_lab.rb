f00a9f000b7a5af7958fda9b9bfb617ef8ee68e0
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-09 04:53:39 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,12 +7,14 @@ def actor
 end
 
 def monopoly
-	monopoly = {:railroads =>
-	{
-	  :pieces => 4,
-	  :names => {},
-	  :rent_in_dollars => {}
-	}}
+	monopoly = {}
+	#:railroads =>
+	#{
+	 # :pieces => 4,
+	  #:names => {},
+	  #:rent_in_dollars => {}
+	#}}
+	
 
 end
 
